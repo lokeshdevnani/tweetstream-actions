e6c31f9662b369b95a8ad2b7db2542046cc80709
Lokesh Devnani <contact@lokeshd.com>
2020-04-20 06:49:58 +0530

    Add some configs

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -14,3 +14,8 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.plural(/rion$/ ,'ria') # criterion => criteria
+  inflect.singular(/ria$/, 'rion') # criteria => criterion
+end
